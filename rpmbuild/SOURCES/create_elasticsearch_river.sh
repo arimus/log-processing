40bfa43c7a5b65ee5e2d90447dbdf44189fa5efc
David Castro <arimus@gmail.com>
2012-04-08 03:13:47 -0700

    fixes to the script that ensures the rabbitmq elasticsearch river is created

--- rpmbuild/SOURCES/create_elasticsearch_river.sh
+++ rpmbuild/SOURCES/create_elasticsearch_river.sh
@@ -6,36 +6,48 @@ PORT=${2:-"9200"}
 echo "Using host=${HOST} port=${PORT}"
 
 # check to see if elasticsearch already has the river created
-RESULT=$(curl -XGET "${HOST}:${PORT}/_river/my_river/_meta" -s|grep elasticsearch|wc -l)
-if [ $RESULT == 3 ]; then
-    echo "Found existing elasticsearch river"
-    exit
-else
-    echo "Creating elasticsearch river"
-    exit
+check_river() {
+    COUNT=$(curl -XGET "${HOST}:${PORT}/_river/my_river/_meta" -s|grep elasticsearch|wc -l)
+    RESULT=$?
+    echo result=${RESULT}
+    if [ $RESULT != 0 ]; then
+        echo "Problem getting river information from elasticsearch"
+        exit 1;
+    fi
+    echo count=${COUNT}
+    if [ $COUNT == 3 ]; then
+        echo "Found existing elasticsearch river"
+        exit 0
+    else
+        echo "Elasticsearch river not found"
+    fi
+}
 
-  # create the river - this is straight from the elasticsearch rabbitmq river doc
-  curl -XPUT "${HOST}:${PORT}/_river/my_river/_meta" -d '{
-      "type" : "rabbitmq",
-      "rabbitmq" : {
-          "host" : "localhost", 
-          "port" : 5672,
-          "user" : "guest",
-          "pass" : "guest",
-          "vhost" : "/",
-          "queue" : "elasticsearch",
-          "exchange" : "elasticsearch",
-          "routing_key" : "elasticsearch",
-          "exchange_type" : "direct",
-          "exchange_durable" : true,
-          "queue_durable" : true,
-          "queue_auto_delete" : false
-      },
-      "index" : {
-          "bulk_size" : 100,
-          "bulk_timeout" : "10ms",
-          "ordered" : false
-      }
-  }'
+check_river
+echo "Creating elasticsearch river"
 
-fi
+# create the river - this is straight from the elasticsearch rabbitmq river doc
+curl -XPUT "${HOST}:${PORT}/_river/my_river/_meta" -d '{
+    "type" : "rabbitmq",
+    "rabbitmq" : {
+        "host" : "localhost", 
+        "port" : 5672,
+        "user" : "guest",
+        "pass" : "guest",
+        "vhost" : "/",
+        "queue" : "elasticsearch",
+        "exchange" : "elasticsearch",
+        "routing_key" : "elasticsearch",
+        "exchange_type" : "direct",
+        "exchange_durable" : true,
+        "queue_durable" : true,
+        "queue_auto_delete" : false
+    },
+    "index" : {
+        "bulk_size" : 100,
+        "bulk_timeout" : "10ms",
+        "ordered" : false
+    }
+}'
+
+check_river
